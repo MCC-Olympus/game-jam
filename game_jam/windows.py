611cb5184529b4d07aeef51c2e0f99627efe9504
Ben Grelk <bjgrelk@mail.mccneb.edu>
2023-01-31 13:54:15 -0600

    Update windows.py

--- game_jam/windows.py
+++ game_jam/windows.py
@@ -4,9 +4,7 @@ from events import *
 from gui import Window, Button, WIDTH
 from values import Defaults
 
-
 volume = Defaults.volume
-
 
 menu = Window(caption="JellySmash Menu")
 menu.elements = {
@@ -15,13 +13,11 @@ menu.elements = {
     "exit": Button("Exit", (WIDTH // 2 - 100, 400, 200, 50), on_click=exit_game),
 }
 
-
 settings = Window(caption="JellySmash Settings")
 settings.elements = {
-    "increase_sound": Button("->", (WIDTH // 2 + 125, 200, 50, 50,), on_click=increase_volume, ),
+    "increase_sound": Button("->", (WIDTH // 2 + 125, 200, 50, 50,), on_click=increase_volume),
     "decrease_sound": Button("<-", (WIDTH // 2 - 175, 200, 50, 50), on_click=decrease_volume),
-    "sound": Button(f"Sound: {Defaults.volume * 100:.0f}", (WIDTH // 2 - 100, 200, 200, 50), on_click=reset_sound, ),
+    "sound": Button(f"Sound: {Defaults.volume * 100:.0f}", (WIDTH // 2 - 100, 200, 200, 50), on_update=update_volume,
+                    on_click=reset_sound),
     "back": Button("Back", (WIDTH // 2 - 100, 400, 200, 50), on_click=to_menu),
 }
-
-settings.elements["sound"].on_update = update_volume
