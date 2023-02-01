9fa64f9a5114e6a9ebfd561ea9a2dab19329ce96
Ben Grelk <bjgrelk@mail.mccneb.edu>
2023-01-31 19:43:55 -0600

    Add demo code
    This commit adds code to demonstrate how to use the various classes and functions to create responsive UI elements.

--- game_jam/main.py
+++ game_jam/main.py
@@ -1,8 +1,26 @@
 """The entry point of the game."""
 
-from windows import menu
+from gui import *
 
-if __name__ == "__main__":
-    print("Welcome to JellySmash!")
 
-    menu.open()
+# This is demo code that shows how to use the classes and functions to create a GUI window
+
+def say_hi(window: Window):
+    """Shows this every time the jar is clicked."""
+    print("hi :D")
+
+
+def rotate(window: Window):
+    """Does this every frame"""
+    window.get_element("spinning jar").angle += 1
+
+
+# Create instance of the window class
+my_window = Window(caption="Demo")
+my_window.elements = {
+    "title": Text("Demo of UI", (WIDTH // 2, HEIGHT // 3, 0, 0), font_size=50, color=(255, 0, 0)),
+    # Add a button called "test" to the window
+    "spinning jar": Button(SPRITE_PATH / "purpleJar.png", (WIDTH // 2, HEIGHT // 2,), on_click=say_hi, on_update=rotate)
+}
+
+my_window.open()
