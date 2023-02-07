1c7f963904dc9ed674702cd36777c2a59eaf9a02
Ben Grelk <bjgrelk@mail.mccneb.edu>
2023-02-07 13:22:09 -0600

    Add audio_processing.py
    This commit adds a file for getting each note of a given wave file and returning the dominant frequency of each note.

--- game_jam/audio_processing.py
+++ game_jam/audio_processing.py
@@ -1,37 +1,39 @@
 """Handles all the processing of audio."""
 
-import wave
-
-import librosa.display
-import pyaudio
-
-
-def play(path, start, end) -> None:
-    # Play a sound file from a given start to end time. Make it echo at the end
-    # of the song.
-    wf = wave.open(path, "rb")
-    p = pyaudio.PyAudio()
-    stream = p.open(
-        format=p.get_format_from_width(wf.getsampwidth()),
-        channels=wf.getnchannels(),
-        rate=wf.getframerate(),
-        output=True,
-    )
-    # make it echo and fade out
-    for i in range(0, 10):
-        wf.setpos(int(start * wf.getframerate()))
-        data = wf.readframes(int((end - start) * wf.getframerate()))
-        stream.write(data)
-        start = end
-        end = end + (end - start) * (i + 1) / 10
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+import librosa
+import numpy as np
 
 
 def get_each_note(path):
-    """Returns the time of each note in a song."""
+    """Returns the time of each note in a song and the frequency at that time."""
     y, sr = librosa.load(path)
     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
-    tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
-    return librosa.frames_to_time(beats, sr=sr)
+    beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)[1]
+
+    times = librosa.frames_to_time(beats, sr=sr)
+    return times, get_frequencies(path, times)
+
+
+def get_frequencies(filename, time_stamps) -> list[float]:
+    """Gets the average frequency of each note."""
+    # Load the audio samples
+    samples, sr = librosa.load(filename)
+
+    avg_frequencies = []
+    for time in time_stamps:
+        # Extract a segment of the audio
+        start = time * sr
+        end = start + sr
+        segment = samples[int(start) : int(end)]
+
+        # Convert the audio to the frequency domain
+        spectrum = np.abs(np.fft.rfft(segment))
+
+        # Calculate the average frequency
+        avg_frequency = np.mean(spectrum)
+
+        # Add the average frequency to the list
+        avg_frequencies.append(avg_frequency)
+
+    # Make the list of floats
+    return [float(frequency) for frequency in avg_frequencies]
