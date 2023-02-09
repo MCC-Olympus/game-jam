418a7339a388b9d885a8abf0293d790a167dc30c
Ben Grelk <bjgrelk@mail.mccneb.edu>
2023-02-09 14:53:34 -0600

    Add constants.py
    This file is for all values that remain the same throughout the game and are used multiple times

---
+++ game_jam/constants.py
@@ -0,0 +1,23 @@
+"""Values that don't change and are used by multiple files"""
+
+from pathlib import Path
+
+import pygame
+
+NAME = "JellySmash"
+
+ASSETS = Path(__file__).parent.parent / "assets"
+SPRITES = ASSETS / "sprites"
+SOUNDS = ASSETS / "sounds"
+
+pygame.display.init()
+WIDTH = pygame.display.Info().current_w
+HEIGHT = pygame.display.Info().current_h
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.HWSURFACE)
+
+FPS = 60
+
+# Type hints
+Coordinate = tuple[int, int]
+RGB = tuple[int, int, int]
+Rect = tuple[int, int, int, int]
